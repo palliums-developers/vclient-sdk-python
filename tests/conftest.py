e80660476fafab049cc706374f62c1d3141f78ed
yinzhiqing <yinzhiqing@163.com>
2021-11-25 10:10:52 +0800

    modified connect worknet; added test of get_account_state

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,21 @@
 import sys
 import os
 
+from enum import Enum
+
+class WorkNet(Enum):
+    TESTNET  = "http://testnet.diem.io"
+    INTERNAL = ""
+    DEVNET   = "http://124.251.110.242:8080"
+    LOCAL    = "http://127.0.0.1:8080"
+
 VIOLAS_CLIENT_SRC_DIR   = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../src/"))
 
 sys.path.append(VIOLAS_CLIENT_SRC_DIR)
 
-#JSON_RPC_URL = "http://47.93.114.230:50001"
-#JSON_RPC_URL = "http://127.0.0.1:8080"
-JSON_RPC_URL = "http://124.251.110.242:8080"
+# set worknet
+node_url = WorkNet.DEVNET
+print("connect to worknet: " + node_url.value)
+
+#get node url
+JSON_RPC_URL = node_url.value
